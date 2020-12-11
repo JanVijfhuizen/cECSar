23f077836e6ccd50082386354739a70bb68eebab
Bonniebon <jan.vijfhuizen@outlook.com>
2020-12-11 13:59:31 +0100

    Added SDL to the game project.

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -4,19 +4,24 @@
 #include "Testing/TestModule.h"
 #include "Testing/TestFactory.h"
 
-int main()
+#include "SDL.h"
+
+int main(int argc, char* argv[])
 {
+	SDL_Init(0);
+
 	cecsar::Cecsar cecsar{};
 	cecsar.Update<TestSystem>();
 	auto& module = cecsar.GetModule<TestModule>();
 
-	cecsar.AddEntity<TestFactory>();
-	cecsar.AddEntity<>();
+	delete [] cecsar.AddEntity<TestFactory>(5);
+	delete [] cecsar.AddEntity<>();
 
 	auto& component = cecsar.AddComponent<TestBlockA>(12);
 	cecsar.RemoveComponent<TestBlockB>(7);
 
 	cecsar.RemoveEntity(0);
 
+	SDL_Quit();
 	return 0;
 }
