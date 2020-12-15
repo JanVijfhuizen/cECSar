3e3b04b405bd3630e16489aaca9b9fddd88be022
Bonniebon <jan.vijfhuizen@outlook.com>
2020-12-15 20:46:02 +0100

    Moved level creation to level generator.

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -12,6 +12,7 @@
 #include "Modules/TimeModule.h"
 #include <iostream>
 #include "Systems/CameraSystem.h"
+#include "Managers/LevelGenerator.h"
 
 int main(int argc, char* argv[])
 {
@@ -26,52 +27,17 @@ int main(int argc, char* argv[])
 	cecsar::Cecsar cecsar{ info };
 
 	// Modules.
+	auto& timeModule = cecsar.GetModule<game::TimeModule>();
 	auto& renderModule = cecsar.GetModule<game::RenderModule>();
+
 	renderModule.zMod = .1;
 	renderModule.zColorFallof = .2f;
 
-	// Component sets.
-	auto& transforms = cecsar.GetSet<game::Transform>();
-	auto& renderers = cecsar.GetSet<game::Renderer>();
+	// Spawn level.
+	const game::LevelGenerator generator;
+	delete [] generator.Generate(cecsar);
 
-	// Setup some nonsense to experiment with.
-	const int32_t WIDTH = 12, HEIGHT = 8;
-	const int32_t* ptrs = cecsar.AddEntity<game::BlockFactory>(WIDTH * HEIGHT);
-
-	const int32_t posMod = renderModule.DEFAULT_IMAGE_SIZE * renderModule.DEFAULT_IMAGE_UPSCALING;
-	const int32_t padding = renderModule.DEFAULT_IMAGE_SIZE / 2;
-
-	// Setup floor.
-	for (int32_t y = 0; y < HEIGHT; ++y)
-	{
-		const int32_t* row = &ptrs[y * WIDTH];
-
-		for (int32_t x = 0; x < WIDTH; ++x)
-		{
-			const int32_t* index = row + x;
-			auto& transform = transforms[*index];
-
-			transform.posLocal.x = x * posMod + x * padding;
-			transform.posLocal.y = y * posMod + y * padding;
-
-			transform.posLocal.z = -.2f * (x + y);
-			transform.rot = x + y;
-
-			auto& renderer = renderers[*index];
-			renderer.color.r = x * 10;
-			renderer.color.g = y * 10;
-			renderer.color.b = abs(transform.posLocal.z) * 10;
-		}
-	}
-	delete [] ptrs;
-
-	const int32_t* ptrsMoving = cecsar.AddEntity<game::UndeadFactory>(2);
-	game::TransformHelper::SetParent(transforms, ptrsMoving[1], ptrsMoving[0]);
-	//delete[] ptrsMoving;
-	transforms.Get(ptrsMoving[0]).posLocal.y = 100;
-
-	auto& timeModule = cecsar.GetModule<game::TimeModule>();
-
+	// Spawn player for testing purposes.
 	const auto& player = cecsar.AddEntity<game::UndeadFactory>(1);
 	cecsar.GetSet<game::Controller>().Get(player[0]).type = game::ControllerType::player;
 	cecsar.AddComponent<game::CameraFollowTarget>(player[0]);
@@ -84,13 +50,6 @@ int main(int argc, char* argv[])
 
 		while (SDL_PollEvent(&event) != 0)
 			;
-
-		const float time = timeModule.GetTime();
-
-		transforms.Get(ptrsMoving[0]).rot += timeModule.GetDeltaTime() * 50;
-		transforms.Get(ptrsMoving[0]).posLocal.x = 200 + sin(time) * 100;
-		transforms.Get(ptrsMoving[0]).posLocal.z = sin(time) * 2;
-		transforms.Get(ptrsMoving[1]).posLocal.y = cos(time) * 100;
 
 		cecsar.Update<game::ControllerSystem>();
 		cecsar.Update<game::MovementSystem>();
