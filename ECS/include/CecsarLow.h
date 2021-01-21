5426af78961c5b67f1d499dd783ecf8162da941d
Jan Vijfhuizen <jan.vijfhuizen@outlook.com>
2021-01-21 18:32:33 +0100

    Made experimental new version of cecsar.
    Ultimately this project was designed to get better at the ECS/DoD workflow, and I'm making this version based on the experience I've had with the previous system.
    
    Currently nothing is commented, so it will look a bit like a mess.

---
+++ ECS/include/CecsarLow.h
@@ -0,0 +1,248 @@
+#pragma once
+#include <cstdint>
+#include <typeindex>
+#include <unordered_map>
+
+#include "SparseSet.h"
+
+namespace revamped
+{
+	struct Entity final
+	{
+		int32_t id = -1;
+		int32_t index = -1;
+
+		constexpr bool operator==(const Entity& other) const;
+	};
+
+	constexpr bool Entity::operator==(const Entity& other) const
+	{
+		return index == other.index && id == other.id;
+	}
+	
+	class Cecsar final
+	{
+	private:
+		class Module
+		{
+		public:
+			Cecsar* cecsar = nullptr;
+
+			virtual ~Module() = default;
+			virtual void Initialize(Cecsar& cecsar);
+		};
+
+		class InternalSystem : public Module
+		{
+		public:
+			virtual void Update() = 0;
+		};
+
+		class InternalFactory : public Module
+		{
+		public:
+			virtual void Construct(int32_t id) = 0;
+		};
+		
+	public:
+		struct Info final
+		{
+			int32_t capacity = 1e3;
+		};
+
+		template <typename Component, typename ...Args>
+		class System : public InternalSystem
+		{
+		public:
+			constexpr void Update() final override;
+
+		private:
+			using Module::cecsar;
+			
+			virtual void OnUpdate(utils::SparseSet<Component>& instances, utils::SparseSet<Args>&...) = 0;
+		};
+
+		class Factory : public InternalFactory
+		{
+		public:
+			inline void Construct(int32_t id) final override;
+
+		private:
+			using Module::cecsar;
+			
+			virtual void OnConstruct(Cecsar& cecsar, int32_t id) = 0;
+		};
+
+		const Info info;
+
+		inline Cecsar(const Info& info = Info());
+		inline ~Cecsar();
+
+		[[nodiscard]] constexpr Entity Spawn();
+		inline void Delete(int32_t index) const;
+		[[nodiscard]] constexpr bool Validate(const Entity& entity) const;
+		
+		template <typename Interface, typename InitType = Interface>
+		constexpr Interface& Get();
+
+		template <typename Component>
+		constexpr utils::SparseSet<Component>& GetSet();
+
+		constexpr int32_t GetGlobalEntityIndex() const;
+
+	private:
+		class AbstractSet : public Module
+		{
+		public:
+			virtual void Remove(int32_t index) = 0;
+		};
+
+		template <typename Component>
+		class Set final : public AbstractSet
+		{
+		public:
+			utils::SparseSet<Component>* components = nullptr;
+			
+			inline ~Set();
+			void Initialize(Cecsar& cecsar) override;
+			
+			constexpr void Remove(int32_t index) override;
+		};
+		
+		enum class ModuleType
+		{
+			System,
+			Factory,
+			Set,
+			Count
+		};
+
+		int32_t _globalEntityIndex = 0;
+		utils::SparseSet<Entity>* _entities = nullptr;
+		std::unordered_map<std::type_index, Module*> _modules[static_cast<int32_t>(ModuleType::Count)]{};
+
+		constexpr static ModuleType Get(InternalSystem* out);
+		constexpr static ModuleType Get(InternalFactory* out);
+	};
+
+	template <typename Component, typename ... Args>
+	constexpr void Cecsar::System<Component, Args...>::Update()
+	{
+		OnUpdate(cecsar->GetSet<Component>(), cecsar->GetSet<Args>()...);
+	}
+
+	template <typename Component>
+	Cecsar::Set<Component>::Set::~Set()
+	{
+		delete components;
+	}
+
+	template <typename Component>
+	void Cecsar::Set<Component>::Initialize(Cecsar& cecsar)
+	{
+		components = new utils::SparseSet<Component>(cecsar.info.capacity);
+	}
+
+	template <typename Interface, typename InitType>
+	constexpr Interface& Cecsar::Get()
+	{
+		Interface* i = nullptr;
+		const auto type = Get(i);
+		Module*& ptr = _modules[static_cast<int32_t>(type)][typeid(Interface)];
+		if(!ptr)
+		{
+			ptr = new InitType;
+			
+			ptr->cecsar = this;
+			ptr->Initialize(*this);
+		}
+
+		auto* interface = static_cast<Interface*>(ptr);
+		return *interface;
+	}
+
+	template <typename Component>
+	constexpr utils::SparseSet<Component>& Cecsar::GetSet()
+	{
+		Module*& ptr = _modules[static_cast<int32_t>(ModuleType::Set)][typeid(Component)];
+		if(!ptr)
+		{
+			ptr = new Set<Component>;
+			ptr->Initialize(*this);
+		}
+
+		auto set = static_cast<Set<Component>*>(ptr);
+		return *set->components;
+	}
+
+	template <typename Component>
+	constexpr void Cecsar::Set<Component>::Remove(const int32_t index)
+	{
+		components->RemoveAt(index);
+	}
+
+	inline void Cecsar::Factory::Construct(int32_t id)
+	{
+		OnConstruct(*cecsar, id);
+	}
+
+	inline Cecsar::Cecsar(const Info& info) : info(info)
+	{
+		_entities = new utils::SparseSet<Entity>(info.capacity);
+	}
+
+	inline Cecsar::~Cecsar()
+	{
+		delete _entities;
+	}
+
+	constexpr Cecsar::ModuleType Cecsar::Get(InternalSystem*)
+	{
+		return ModuleType::System;
+	}
+
+	constexpr Cecsar::ModuleType Cecsar::Get(InternalFactory*)
+	{
+		return ModuleType::Factory;
+	}
+
+	constexpr Entity Cecsar::Spawn()
+	{
+		const int32_t index = _entities->Add(
+			{
+				_globalEntityIndex++
+			});
+		
+		auto& entity = _entities->Get(index);
+		entity.index = index;
+		return entity;
+	}
+
+	inline void Cecsar::Delete(const int32_t index) const
+	{
+		_entities->RemoveAt(index);
+
+		const auto& sets = _modules[static_cast<int32_t>(ModuleType::Set)];
+		for (const auto& pair : sets)
+		{
+			auto* const set = static_cast<AbstractSet*>(pair.second);
+			set->Remove(index);
+		}
+	}
+
+	inline void Cecsar::Module::Initialize(Cecsar&)
+	{
+	}
+
+	constexpr bool Cecsar::Validate(const Entity& entity) const
+	{
+		if (entity.id == -1)
+			return false;
+		return _entities->Get(entity.index).id == entity.id;
+	}
+
+	constexpr int32_t Cecsar::GetGlobalEntityIndex() const
+	{
+		return _globalEntityIndex;
+	}
+}
--- Game/Game.cpp
+++ Game/Game.cpp
@@ -1,7 +1,50 @@
-#include <Engine.h>
+//#include <Engine.h>
+#include <CecsarLow.h>
+
+struct SomeComponent final
+{
+	int32_t x, y;
+};
+
+struct SomeOtherComponent final
+{
+	int32_t a, b, c;
+};
+
+class SomeFactory final : public revamped::Cecsar::Factory
+{
+private:
+	void OnConstruct(revamped::Cecsar& cecsar, int32_t id) override;
+};
+
+void SomeFactory::OnConstruct(revamped::Cecsar& cecsar, const int32_t id)
+{
+	auto& component = cecsar.GetSet<SomeComponent>().Insert(id);
+	component.x = 500;
+}
+
+class SomeSystem final : public revamped::Cecsar::System<SomeComponent, SomeOtherComponent>
+{
+	void OnUpdate(utils::SparseSet<SomeComponent>& instances, utils::SparseSet<SomeOtherComponent>&) override;
+};
+
+void SomeSystem::OnUpdate(utils::SparseSet<SomeComponent>& instances, utils::SparseSet<SomeOtherComponent>&)
+{
+	for (auto& instance : instances)
+		instance.x--;
+}
 
 int main(int argc, char* argv[])
 {
-	game::Engine::Run();
+	//game::Engine::Run();
+	revamped::Cecsar cecsar{};
+
+	const auto entity = cecsar.Spawn();
+	cecsar.Get<SomeFactory>().Construct(entity.index);
+	
+	cecsar.Delete(entity.index);
+
+	cecsar.Get<SomeSystem>().Update();
+	
 	return 0;
 }
